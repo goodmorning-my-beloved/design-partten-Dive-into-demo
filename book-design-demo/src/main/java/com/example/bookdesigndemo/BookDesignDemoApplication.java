e0cce5f27394202261543446ba460b6761300296
1271623149 <1271623149@qq.com>
2022-12-09 10:45:17 +0800

    add:初始化工程

---
+++ book-design-demo/src/main/java/com/example/bookdesigndemo/BookDesignDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookdesigndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDesignDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookDesignDemoApplication.class, args);
+    }
+
+}
---
+++ book-design-demo/src/test/java/com/example/bookdesigndemo/BookDesignDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookdesigndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDesignDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
