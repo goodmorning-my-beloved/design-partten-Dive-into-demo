d3516e2a097126c82464cb10fed2453f7cf7ba20
1271623149 <1271623149@qq.com>
2022-12-09 17:37:44 +0800

    add:责任链模式和策略模式

---
+++ book-design-demo/src/main/java/com/example/bookdesigndemo/behavior/strategypattern/PayStrategy.java
@@ -0,0 +1,9 @@
+package com.example.bookdesigndemo.behavior.strategypattern;
+
+/**
+ * Common interface for all strategies.
+ */
+public interface PayStrategy {
+    boolean pay(int paymentAmount);
+    void collectPaymentDetails();
+}
